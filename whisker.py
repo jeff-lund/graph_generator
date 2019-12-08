ce3afad72666df3e4f4c2b0fda86dc4222d31847
jeff-lund <jdl3@pdx.edu>
2019-12-08 13:02:30 -0800

    adding whisker plots

---
+++ whisker.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+class Data:
+    def __init__(self, d):
+        self.foreground = d[0]
+        self.background = d[1]
+        self.uninterf = float(d[4])
+        self.interf = float(d[5])
+        tmp = d[6].replace('%', '')
+        self.delta = float(tmp)
+
+with open('raw_data.csv', 'r') as f:
+    data = [line.strip().split(',') for line in f]
+    data = [Data(d) for d in data[1:]]
+    data = [d for d in data 
+            if d.foreground != 'fio' 
+            and d.background != 'fio']
+victim = {d.foreground:[] for d in data}
+perp = {d.background:[] for d in data}
+
+for d in data:
+    victim[d.foreground].append(d.delta)
+    perp[d.background].append(d.delta)
+
+labels = sorted(list({d.foreground for d in data}))
+vicplot = [victim[l] for l in labels]
+perplot = [perp[l] for l in labels]
+
+plt.title("Interference when benchmark program is the victim")
+plt.boxplot(vicplot, labels=labels)
+plt.xticks(rotation='45')
+plt.tight_layout()
+plt.savefig('victim.png', format='png')
+plt.clf()
+
+plt.title("Interference when benchmark program is the perpatraor")
+plt.boxplot(perplot, labels=labels)
+plt.xticks(rotation='45')
+plt.tight_layout()
+plt.savefig('perp.png', format='png')
+
